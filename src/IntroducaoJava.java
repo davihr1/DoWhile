790fcb3ce6975509ed3243cb2d47338b3271d2b4
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2025-02-28 18:47:58 -0300

    dowhile&for

---
+++ src/IntroducaoJava.java
@@ -0,0 +1,9 @@
+import static java.lang.System.*;
+
+public class IntroducaoJava {
+    public static void main(String[] args) {
+        for (int i = 0; i<=5; i++) {
+            out.println(i);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 import static java.lang.System.out;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,9 +8,48 @@ public class Main {
     public static void main(String[] args) {
         int numero = 1;
 
-        while (numero<=5) {
-            out.println(numero);
-            numero++;
-        }
+        Scanner teclado = new Scanner(System.in);
+        int op;
+
+        do {
+            out.println("--------------");
+            out.println("Menu Principal");
+            out.println("--------------");
+            out.println("Selecione uma das opçoes abaixo");
+            out.println("1 - Aluno");
+            out.println("2 - Professor");
+            out.println("3 - Coordenador");
+            out.println("0 - Sair");
+
+
+            out.println("Digite uma Opção: ");
+            op = teclado.nextInt();
+
+            if (op == 0)
+                break;
+
+            switch (op) {
+                case 1:
+                    out.println("Você escolheu Aluno");
+                break;
+
+                case 2:
+                    out.println("Você escolheu Professor");
+                break;
+
+                case 3:
+                    out.println("Você escolheu Coordenador");
+                break;
+
+                default:
+                    out.println("Você escolheu uma opção invalida");
+
+
+
+            }
+
+        } while (true);
+
+        out.println("Fim");
     }
 }
