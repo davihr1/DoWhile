a4fe01e47468ef3c5f581fba95150335efdce1d2
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2025-02-28 17:14:31 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import static java.lang.System.out;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        while (numero<=5) {
+            out.println(numero);
+            numero++;
+        }
+    }
+}
