965cbc46af92981e072d1fe43ce56652af3baa39
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2025-03-03 15:47:33 -0300

    Matrizes direto na declaração

--- src/Arrays.java
+++ src/Arrays.java
@@ -6,16 +6,10 @@ public class Arrays {
             System.out.println(valor);
 
         // Arrays Bidimensionais
-        int[][] matriz = new int[2][3];
+
+//                        Linha   / Coluna
+        int[][] matriz = {{0,1,2},{4,5,6}};
         int i,j;
-
-        matriz[0][0] = 1;
-        matriz[0][1] = 2;
-        matriz[0][2] = 3;
-
-        matriz[1][0] = 4;
-        matriz[1][1] = 5;
-        matriz[1][2] = 6;
 
         for (i=0;i<2;i++){
             for (j=0;j<3;j++)
