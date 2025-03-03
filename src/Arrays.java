68ba4fb35f6483f7ecdcf0f3276dbee332b9843a
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2025-03-03 14:53:14 -0300

    Variação de Arrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,9 +1,6 @@
 public class Arrays {
     public static void main(String[] args) {
-        int[] nota = new int[3];
-        nota[0] = 9;
-        nota[1] = 8;
-        nota[2] = 10;
+        int[] nota = {9,8,10};
 
         int i;
         for (i=0; i<3; i++)
