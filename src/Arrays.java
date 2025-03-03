245a532317b9e10294ddc058310bb6387f440cc1
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2025-03-03 15:07:21 -0300

    NewFor

--- src/Arrays.java
+++ src/Arrays.java
@@ -2,8 +2,8 @@ public class Arrays {
     public static void main(String[] args) {
         int[] nota = {9,8,10};
 
-        int i;
-        for (i=0; i<3; i++)
-            System.out.println(nota[i]);
+        for (int valor: nota)
+            System.out.println(valor);
+
     }
 }
